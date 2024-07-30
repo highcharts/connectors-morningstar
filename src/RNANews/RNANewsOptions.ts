85a1d22392f0561b09e6211cbbe2f53d9ab31b9f
Eskil Gjerde Sviggum <eskilsviggum@gmail.com>
2024-07-30 10:42:01 +0200

    Minor refactor

--- src/RNANews/RNANewsOptions.ts
+++ src/RNANews/RNANewsOptions.ts
@@ -1,3 +1,22 @@
+/* *
+ *
+ *  (c) 2009-2024 Highsoft AS
+ *
+ *  License: www.highcharts.com/license
+ *
+ *  !!!!!!! SOURCE GETS TRANSPILED BY TYPESCRIPT. EDIT TS FILE ONLY. !!!!!!!
+ *
+ *  Authors:
+ *  - Eskil Gjerde Sviggum
+ *
+ * */
+
+
+/* *
+ *
+ *  Imports
+ *
+ * */
 import type LocalizationOptions from '../Shared/LocalizationOptions';
 import MorningstarOptions, { MorningstarConverterOptions, MorningstarSecurityOptions } from "../Shared/MorningstarOptions";
 import RNANewsJSON from './RNANewsJSON';
@@ -5,32 +24,32 @@ import RNANewsJSON from './RNANewsJSON';
 export interface RNANewsOptions extends RNANewsConverterOptions, MorningstarOptions {
 
     /**
-   * Security to retrieve.
-   */
+     * Security to retrieve.
+     */
     security?: MorningstarSecurityOptions;
 
     /**
-   * The start date of the time series.
-   * Should be either a UNIX timestamp,
-   * or a string formatted as `yyyy-MM-dd`.
-   */
+     * The start date of the time series.
+     * Should be either a UNIX timestamp,
+     * or a string formatted as `yyyy-MM-dd`.
+     */
     startDate?: number | string;
 
     /**
-   * The end date of the time series.
-   * Should be either a UNIX timestamp,
-   * or a string formatted as `yyyy-MM-dd`.
-   */
+     * The end date of the time series.
+     * Should be either a UNIX timestamp,
+     * or a string formatted as `yyyy-MM-dd`.
+     */
     endDate?: number | string;
 
     /**
-   * The maximum number of announcements to load.
-   */
+     * The maximum number of announcements to load.
+     */
     maxStories?: number;
 
     /**
-   * Localization options.
-   */
+     * Localization options.
+     */
     localization?: LocalizationOptions;
 
 }
