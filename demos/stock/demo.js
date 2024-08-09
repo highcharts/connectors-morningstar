243450dd87cbfe833b0142fb0c2d9afb94efe27b
bre1470 <40056287+bre1470@users.noreply.github.com>
2024-08-09 11:20:35 +0200

    Added demos to NPM package.

--- demos/stock/demo.js
+++ demos/stock/demo.js
@@ -1,13 +1,59 @@
-Dashboards.board('container', {
-    dataPool: {
-        connectors: [{
-            id: 'Time Series',
-            type: 'MorningstarTimeSeries',
-            options: {
-                postman: {
-                    environmentURL: '/tmp/Environment.json'
-                }
-            }
-        }]
-    }
-});
+document.getElementById('postman-json').addEventListener(
+    'change',
+    async function (
+        evt
+    ) {
+        const target = evt.target;
+        const postmanJSON = JSON.parse(await target.files[0]?.text());
+
+        target.parentNode.style.display = 'none';
+
+        Dashboards.board('container', {
+            dataPool: {
+                connectors: [{
+                    id: 'morningstar-dividend',
+                    type: 'MorningstarTimeSeries',
+                    options: {
+                        postman: {
+                            environmentJSON: postmanJSON
+                        },
+                        series: 'Dividend',
+                        securities: [{
+                            id: 'F0GBR06KY1',
+                            idType: 'MSID'
+                        }, {
+                            id: 'LU0011963245USD',
+                            idType: 'ISIN'
+                        }],
+                        startDate: '2020-01-01',
+                        endDate: '2024-12-31'
+                    }
+                }]
+            },
+            gui: {
+                layouts: [{
+                    id: 'layout-1',
+                    rows: [{
+                        cells: [{
+                            id: 'dashboard-col-0'
+                        }]
+                    }]
+                }]
+            },
+            components: [{
+                renderTo: 'dashboard-col-0',
+                connector: {
+                    id: 'morningstar-dividend',
+                    columnAssignment: [{
+                        seriesId: 'F0GBR06KY1',
+                        data: ['Date', 'F0GBR06KY1']
+                    }, {
+                        seriesId: 'LU0011963245USD',
+                        data: ['Date', 'LU0011963245USD']
+                    }]
+                },
+                type: 'Highcharts',
+                chartConstructor: 'stockChart',
+            }]
+        });
+    });
