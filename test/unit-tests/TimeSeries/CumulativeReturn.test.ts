48c3f07d58dbd0293508d4156af3faa70e78aaa6
bre1470 <40056287+bre1470@users.noreply.github.com>
2024-08-16 18:59:29 +0200

    Added unit test for cumulative return.

---
+++ test/unit-tests/TimeSeries/CumulativeReturn.test.ts
@@ -0,0 +1,46 @@
+import * as Assert from 'node:assert/strict';
+import * as MC from '../../../code/connectors-morningstar.src';
+
+export async function cumulativeReturnLoad (
+    api: MC.Shared.MorningstarAPIOptions
+) {
+    const connector = new MC.TimeSeriesConnector({
+        api,
+        currencyId: 'EUR',
+        endDate: '2020-12-31',
+        securities: [{
+            id: 'F0GBR04S23',
+            idType: 'MSID'
+        }],
+        series: {
+            type: 'CumulativeReturn'
+        },
+        startDate: '2020-01-01'
+    });
+
+    Assert.ok(
+        connector instanceof MC.TimeSeriesConnector,
+        'Connector should be instance of TimeSeries class.'
+    );
+
+    Assert.ok(
+        connector.converter instanceof
+        MC.TimeSeriesConverters.CumulativeReturnSeriesConverter,
+        'Converter should be instance of TimeSeries CumulativeReturnSeriesConverter.'
+    );
+
+    await connector.load();
+
+    Assert.deepStrictEqual(
+        connector.table.getColumnNames(),
+        ['Date', connector.options.securities?.[0].id],
+        'Connector table should exist of expected columns.'
+    );
+
+    Assert.strictEqual(
+        connector.table.getRowCount(),
+        366,
+        'Connector table should have 366 cumulative return rows.'
+    );
+
+}
