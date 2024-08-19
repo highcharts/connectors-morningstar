746a5d6615da03ef89fa68072cc84bf67a971e84
Eskil Gjerde Sviggum <eskilsviggum@gmail.com>
2024-08-19 12:56:58 +0200

    Add abstract class `TimeSeriesConverter`

---
+++ src/TimeSeries/Converters/TimeSeriesConverter.ts
@@ -0,0 +1,65 @@
+/* *
+ *
+ *  (c) 2009-2024 Highsoft AS
+ *
+ *  License: www.highcharts.com/license
+ *
+ *  !!!!!!! SOURCE GETS TRANSPILED BY TYPESCRIPT. EDIT TS FILE ONLY. !!!!!!!
+ *
+ *  Authors:
+ *  - Eskil Gjerde Sviggum
+ *
+ * */
+
+
+'use strict';
+
+
+/* *
+ *
+ *  Imports
+ *
+ * */
+
+
+import MorningstarConverter from '../../Shared/MorningstarConverter';
+import MorningstarURL from '../../Shared/MorningstarURL';
+
+
+/* *
+ *
+ *  Class
+ *
+ * */
+
+
+export abstract class TimeSeriesConverter extends MorningstarConverter {
+
+    /* *
+     *
+     *  Properties
+     *
+     * */
+
+    public abstract path: string;
+
+    /* *
+     *
+     *  Functions
+     *
+     * */
+
+    // eslint-disable-next-line  @typescript-eslint/no-unused-vars
+    public decorateURL (url: MorningstarURL) {}
+
+}
+
+
+/* *
+ *
+ *  Default Export
+ *
+ * */
+
+
+export default TimeSeriesConverter;
