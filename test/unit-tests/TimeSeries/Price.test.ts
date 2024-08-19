82308b0049e025e3b490edb232d8ca8a84b80ab6
Eskil Gjerde Sviggum <eskilsviggum@gmail.com>
2024-08-19 11:11:27 +0200

    Add unit test for loading price

---
+++ test/unit-tests/TimeSeries/Price.test.ts
@@ -0,0 +1,47 @@
+import * as Assert from 'node:assert/strict';
+import * as MC from '../../../code/connectors-morningstar.src';
+
+export async function priceLoad (
+    api: MC.Shared.MorningstarAPIOptions
+) {
+    const connector = new MC.TimeSeriesConnector({
+        api,
+        currencyId: 'EUR',
+        securities: [{
+            id: 'F0GBR04S23',
+            idType: 'MSID'
+        }],
+        series: {
+            type: 'Price',
+            priceType: 'PRICE'
+        },
+        startDate: '2020-01-16',
+        endDate: '2020-01-31'
+    });
+
+    Assert.ok(
+        connector instanceof MC.TimeSeriesConnector,
+        'Connector should be instance of TimeSeries class.'
+    );
+
+    Assert.ok(
+        connector.converter instanceof
+        MC.TimeSeriesConverters.PriceSeriesConverter,
+        'Converter should be instance of TimeSeries PriceSeriesConverter.'
+    );
+
+    await connector.load();
+
+    Assert.deepStrictEqual(
+        connector.table.getColumnNames(),
+        ['Date', connector.options.securities?.[0].id],
+        'Connector table should exist of expected columns.'
+    );
+
+    Assert.strictEqual(
+        connector.table.getRowCount(),
+        12,
+        'Connector table should have 12 expected rating rows.'
+    );
+
+}
