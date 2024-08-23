3108905268d3fdfa3ffb4bae4648444ec31b503a
Eskil Gjerde Sviggum <eskilsviggum@gmail.com>
2024-08-23 15:18:34 +0200

    Add unit test for OHLCV load

---
+++ test/unit-tests/TimeSeries/OHLCV.test.ts
@@ -0,0 +1,46 @@
+import * as Assert from 'node:assert/strict';
+import * as MC from '../../../code/connectors-morningstar.src';
+
+export async function ohlcvLoad (
+    api: MC.Shared.MorningstarAPIOptions
+) {
+    const connector = new MC.TimeSeriesConnector({
+        api,
+        currencyId: 'EUR',
+        securities: [{
+            id: 'XIUSA000O2',
+            idType: 'SECID'
+        }],
+        series: {
+            type: 'OHLCV'
+        },
+        startDate: '2020-01-16',
+        endDate: '2020-01-31'
+    });
+
+    Assert.ok(
+        connector instanceof MC.TimeSeriesConnector,
+        'Connector should be instance of TimeSeries class.'
+    );
+
+    Assert.ok(
+        connector.converter instanceof
+        MC.TimeSeriesConverters.OHLCVSeriesConverter,
+        'Converter should be instance of TimeSeries OHLCVSeriesConverter.'
+    );
+
+    await connector.load();
+
+    Assert.deepStrictEqual(
+        connector.table.getColumnNames(),
+        ['Date', 'Id', 'Open', 'High', 'Low', 'Close', 'Value'],
+        'Connector table should exist of expected columns.'
+    );
+
+    Assert.strictEqual(
+        connector.table.getRowCount(),
+        12,
+        'Connector table should have 12 expected OHLCV rows.'
+    );
+
+}
