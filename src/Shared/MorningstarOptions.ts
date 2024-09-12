8dcffe5c6daf78e4cdc2a9f8bcf5b8a68dae9c39
bre1470 <40056287+bre1470@users.noreply.github.com>
2024-09-12 11:34:25 +0200

    Added Morningstar holding options.

--- src/Shared/MorningstarOptions.ts
+++ src/Shared/MorningstarOptions.ts
@@ -99,6 +99,51 @@ export interface MorningstarConverterOptions extends External.DataConverterOptio
 }
 
 
+export interface MorningstarHoldingAmountOptions extends MorningstarSecurityOptions {
+
+    /**
+     * Holding amount.
+     */
+    amount: number;
+
+    /**
+     * Name of holding.
+     */
+    name?: string;
+
+}
+
+
+export interface MorningstarHoldingWeightOptions extends MorningstarSecurityOptions {
+
+    /**
+     * Name of holding.
+     */
+    name?: string;
+
+    /**
+     * Holding weight.
+     */
+    weight: (number|string);
+
+}
+
+
+export interface MorningstarHoldingValueOptions extends MorningstarSecurityOptions {
+
+    /**
+     * Name of holding.
+     */
+    name?: string;
+
+    /**
+     * Holding value.
+     */
+    value: number;
+
+}
+
+
 export interface MorningstarOptions extends External.DataConnectorOptions {
 
     /**
@@ -151,6 +196,38 @@ export interface MorningstarSecurityOptions {
      */
     idType: string;
 
+    /**
+     * Type of security.
+     */
+    type?: (string|MorningstarSecurityType);
+
+}
+
+
+/* *
+ *
+ *  Enums
+ *
+ * */
+
+
+export enum MorningstarSecurityType {
+    'Bond' = 'BD',
+    '529 Portfolio' = 'CT',
+    'Cash' = 'CASH',
+    'Category Average' = 'CA',
+    'Closed-End Fund' = 'FC',
+    'Economics Series' = 'EI',
+    'Exchange-Traded Fund' = 'FE',
+    'Index' = 'XI',
+    'Insurance Product Fund' = 'FV',
+    'Money Market Fund' = 'FM',
+    'Open-End Fund' = 'FO',
+    'Separate Account' = 'SA',
+    'Stock' = 'ST',
+    'UK LP SubAccounts' = 'VA',
+    'Unit Investment Trust' = 'FI',
+    'Variable Annuity' = 'V1'
 }
 
 
