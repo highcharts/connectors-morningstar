022cc398efe7c2c10ae72c61ce04d503d0d063f6
bre1470 <40056287+bre1470@users.noreply.github.com>
2024-09-13 16:31:42 +0200

    Added entry point for CLI API docs.

---
+++ src/api.d.ts
@@ -0,0 +1,35 @@
+/* *
+ *
+ *  (c) 2009-2024 Highsoft AS
+ *
+ *  License: www.highcharts.com/license
+ *
+ *  !!!!!!! SOURCE GETS TRANSPILED BY TYPESCRIPT. EDIT TS FILE ONLY. !!!!!!!
+ *
+ *  Authors:
+ *  - Sophie Bremer
+ *
+ * */
+
+
+/* *
+ *
+ *  Imports
+ *
+ * */
+
+
+import RNANewsOptions from './RNANews/RNANewsOptions';
+import TimeSeriesOptions from './TimeSeries/TimeSeriesOptions';
+
+
+/* *
+ *
+ *  API Options
+ *
+ * */
+
+interface Options {
+    RNANews: RNANewsOptions;
+    TimeSeries: TimeSeriesOptions;
+}
