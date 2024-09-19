a8f5de0b0215247a873080e479c12e2f8f23324d
Eskil Gjerde Sviggum <eskilsviggum@gmail.com>
2024-09-19 10:31:24 +0200

    Add load integration test for risk score

--- test/unit-tests/RiskScore/RiskScore.test.ts
+++ test/unit-tests/RiskScore/RiskScore.test.ts
@@ -1,5 +1,63 @@
 import * as Assert from 'node:assert/strict';
+import * as MC from '../../../code/connectors-morningstar.src';
 import RiskScoreJSON from '../../../src/RiskScore/RiskScoreJSON';
+
+export async function riskScoreLoad (
+    api: MC.Shared.MorningstarAPIOptions
+) {
+    const connector = new MC.RiskScoreConnector({
+        api,
+        portfolios: [
+            {
+                name: 'TestPortfolio1',
+                currency: 'USD',
+                totalValue: 100,
+                holdings: [
+                    {
+                        id: 'F00000VCTT',
+                        idType: 'SecurityID',
+                        weight: 50
+                    },
+                    {
+                        id: 'AAPL',
+                        idType: 'TradingSymbol',
+                        weight: 50
+                    }
+                ]
+            }
+        ]
+      });
+  
+      Assert.ok(
+          connector instanceof MC.RiskScoreConnector,
+          'Connector should be instance of RiskScoreConnector class.'
+      );
+  
+      Assert.ok(
+          connector.converter instanceof
+          MC.RiskScoreConverter,
+          'Converter should be instance of RiskScoreConverter.'
+      );
+  
+      await connector.load();
+  
+      Assert.deepStrictEqual(
+          connector.table.getColumnNames(),
+          ['TestPortfolio1_EffectiveDate',
+            'TestPortfolio1_RiskScore',
+            'TestPortfolio1_AlignmentScore',
+            'TestPortfolio1_RSquared',
+            'TestPortfolio1_RetainedWeightProxied',
+            'TestPortfolio1_ScoringMethodUsed'],
+          'Connector table should exist of expected columns.'
+      );
+  
+      Assert.strictEqual(
+          connector.table.getRowCount(),
+          1,
+          'Connector table should have one row.'
+      );
+}
 
 export function riskScoreResponseValidation () {
     const exampleResponse = {
