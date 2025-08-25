9e1fc958b092da5845f2d568b350d038ac498fff
Kamil Musiałowski <105275628+kamil-musialowski@users.noreply.github.com>
2025-08-25 14:28:02 +0200

    Added Holdings converter to XRay US. (#130)

--- src/Shared/USConverters/index.ts
+++ src/Shared/USConverters/index.ts
@@ -18,6 +18,7 @@
 import CreditQualityConverter from './CreditQualityConverter';
 import EquityStyleConverter from './EquityStyleConverter';
 import FixedIncomeStyleConverter from './FixedIncomeStyleConverter';
+import HoldingsConverter from './HoldingsConverter';
 
 
 /* *
@@ -39,6 +40,7 @@ import FixedIncomeStyleConverter from './FixedIncomeStyleConverter';
 export * from './CreditQualityConverter';
 export * from './EquityStyleConverter';
 export * from './FixedIncomeStyleConverter';
+export * from './HoldingsConverter';
 
 /* *
  *
@@ -50,5 +52,6 @@ export * from './FixedIncomeStyleConverter';
 export default {
     CreditQualityConverter,
     EquityStyleConverter,
-    FixedIncomeStyleConverter
+    FixedIncomeStyleConverter,
+    HoldingsConverter
 };
