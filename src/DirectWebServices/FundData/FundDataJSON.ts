5a9490833bb2bffa726d853e4109cb7d21d97f91
DJTechnoo <askeltechno@hotmail.com>
2025-11-21 13:16:28 +0100

    POC Added AssetAllocationBreakdown.

---
+++ src/DirectWebServices/FundData/FundDataJSON.ts
@@ -0,0 +1,47 @@
+/* *
+ *
+ *  (c) 2009-2025 Highsoft AS
+ *
+ *  License: www.highcharts.com/license
+ *
+ *  !!!!!!! SOURCE GETS TRANSPILED BY TYPESCRIPT. EDIT TS FILE ONLY. !!!!!!!
+ *
+ *  Authors:
+ *  - Jedrzej Ruta
+ *
+ * */
+
+
+'use strict';
+
+
+/* *
+ *
+ *  Namespace
+ *
+ * */
+
+
+export interface InvestmentResponse {
+    identifiers: {
+        performanceId: string;
+    };
+    metadata: {
+        requestId: string;
+        time: string;
+        messages?: Array<Message>;
+    };
+}
+
+export interface Message {
+    type: string;
+    investments?: Array<InvestmentMessage>;
+}
+
+export interface InvestmentMessage {
+    id?: string;
+    idType?: string;
+    status?: string;
+    datapointId?: Array<string>;
+    errorCode?: string;
+}
