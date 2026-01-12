5911d3c7a70e73936b00da83327a1e302a421451
Kamil Musiałowski <kamil.musialowski@blacklabel.net>
2026-01-12 18:36:55 +0100

    Added demo.

---
+++ demos/dws-prospectus-fees/demo.js
@@ -0,0 +1,53 @@
+import { getPostmanFile } from '../utils/postman-localstorage.js';
+
+getPostmanFile(displayProspectusFees);
+
+const loadingLabel = document.getElementById('loading-label');
+
+async function displayProspectusFees (postmanJSON) {
+   const board = Dashboards.board('container', {
+    dataPool: {
+        connectors: [{
+            id: 'prospectus-fees',
+            type: 'MorningstarDWSInvestments',
+            postman: {
+                environmentJSON: postmanJSON['postmanEnvironmentDWS']
+            },
+            security: {
+                id: '0P00000FIA'
+            },
+            converters: {
+                ProspectusFees: {}
+            },
+            dataModifier: {
+                type: 'Invert'
+            }
+        }]
+    },
+    components: [{
+        renderTo: 'container',
+        type: 'Grid',
+        connector: {
+            id: 'prospectus-fees',
+            dataTableKey: 'ProspectusFees'
+        },
+        title: 'Prospectus Fees',
+        gridOptions: {
+            header: [{
+                columnId: 'columnIds',
+                format: 'Prospectus Fees'
+            }, {
+                columnId: '0',
+                format: 'Values'
+            }]
+        }
+    }]
+});
+
+    board.dataPool
+        .getConnector('prospectus-fees')
+        .then(() => {
+            loadingLabel.style.display = 'none';
+    });
+
+}
